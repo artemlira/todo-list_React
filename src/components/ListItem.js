cab6188f105abe205b0d4614724e37ff800e5e9e
Lira Artem <artemlira84@gmail.com>
2022-09-19 20:23:20 +0300

    добавил иконки выполнения задания и удаления

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,32 +1,40 @@
 import React, { useContext } from "react";
 import { Context } from "../Context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
 
 
 
 export default function ListItem({ id, title, completed }) {
-   const { toggleTodo } = useContext(Context);
-   const { removeTodo } = useContext(Context);
+  const { toggleTodo } = useContext(Context);
+  const { removeTodo } = useContext(Context);
 
-   const statusToDoItem = () => {
-      if (completed) {
-         return 'todo__item todo__completed'
-      } else {
-         return 'todo__item'
-      }
-   }
-   return (
-      <>
+  const statusToDoItem = () => {
+    if (completed) {
+      return 'todo__item todo__completed'
+    } else {
+      return 'todo__item'
+    }
+  }
+  return (
+    <>
 
-         <li className={statusToDoItem()}
+      <li className={statusToDoItem()}>
+        {
+          completed ?
+            <FontAwesomeIcon icon={faCircleCheck} className="delete" onClick={() => toggleTodo(id)} />
+            :
+            <FontAwesomeIcon icon={faCircle} className="delete" onClick={() => toggleTodo(id)} />
+        }
+        {title}
+        <div>
+          <FontAwesomeIcon icon={faTrash} className="delete" onClick={() => removeTodo(id)} />
+        </div>
 
-         > {title}
-            <div>
-               <span className="delete" onClick={() => toggleTodo(id)}>complete</span>
-               <span className="delete" onClick={() => removeTodo(id)}> delete</span>
-            </div>
+      </li>
+    </>
 
-         </li>
-      </>
-
-   )
+  )
 }
